d48405312efe062ec2d55f89467b33489a7dd4ad
Subham <growlyxdev@gmail.com>
2022-02-14 09:56:04 -0500

    Remove JvmOverloads annotation as flavor will only be used in Kotlin applications

--- src/main/kotlin/gg/scala/flavor/FlavorOptions.kt
+++ src/main/kotlin/gg/scala/flavor/FlavorOptions.kt
@@ -3,11 +3,11 @@ package gg.scala.flavor
 import java.util.logging.Logger
 
 /**
+ * Contains several configuration options for a flavor instance.
+ * 
  * @author GrowlyX
  * @since 1/2/2022
  */
-data class FlavorOptions
-@JvmOverloads
-constructor(
+data class FlavorOptions(
     val logger: Logger = Logger.getAnonymousLogger()
 )
